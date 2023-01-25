5e84401a9eefd1ee05c6c4fdd182753f5982f368
Domains18 <gibsonsgibson88@gmail.com>
2023-01-25 22:01:06 +0300

    server

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const colors = require('colors');
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 8000;
+
+const server = express();
 
