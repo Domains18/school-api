d2cefcdfb34169b0186c6c9b25c2e041a8bd69d8
Domains18 <gibsonsgibson88@gmail.com>
2023-01-26 17:12:34 +0300

    login route

--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -1,4 +1,15 @@
 const router = require('express').Router();
 
 const { Student, Teacher, Guardian } = require('../models/registerSchema');
-const {teacherProfile, studentProfile, guardianProfile} = require('../models/profileSchema');
+const {teacherProfile, studentProfile, guardianProfile, StudentProfile} = require('../models/profileSchema');
+
+router.post('/student', async (req, res) => {
+    const { email, password } = req.body;
+    let student = await Student.findOne({ email, password });
+    if (student) {
+        let studentProfile = await StudentProfile.findOne({ studentId: student._id });
+        return res.json({ data: studentProfile, profile: studentProfile });
+    } else return res.status(404).json("Student not found");
+});
+
+
