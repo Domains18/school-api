c18f68f6cfbe8899f1712c858381d22f3694b422
Domains18 <gibsonsgibson88@gmail.com>
2023-01-26 15:54:58 +0300

    registerSchema

---
+++ models/registerSchema.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const student = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+});
+const teacher = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+});
+
+
