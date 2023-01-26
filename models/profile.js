1db5f1adb4b026d56cd4c80660b931334d4344de
Domains18 <gibsonsgibson88@gmail.com>
2023-01-26 15:02:39 +0300

    schema

--- models/profile.js
+++ models/profile.js
@@ -2,6 +2,34 @@ const mongoose = require('mongoose');
 
 const teacherProfile = new mongoose.Schema({
     teacherId: {
-        type: 
-    }
-})
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Teacher',
+        required: true
+    },
+    teacherName: {
+        type: String,
+        required: true
+    },
+    students: [
+        {
+            studentId: { type: mongoose.Schema.Types.ObjectId, ref: 'Students' },
+        }
+    ],
+    memo: [{
+        id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Memo'
+        }
+    }],
+    courses: [
+        {
+            courseId: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Course',
+                required: true
+            }
+        }
+    ]
+});
+
+
