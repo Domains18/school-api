147cbd8cf6c972179c230b183d839dc1b305a7f0
Domains18 <gibsonsgibson88@gmail.com>
2023-01-26 16:30:41 +0300

    courseSchema

---
+++ models/courseSchema.ts
@@ -0,0 +1,71 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const courseSchema = new Schema({
+    teacherId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Teacher',
+        required: true
+    },
+    teacherName: {
+        type: String,
+    },
+    field: {
+        type: String,
+    },
+    semester: {
+        type: String,
+    },
+    courseName: {
+        type: String,
+    },
+    courseCode: {
+        type: String,
+    },
+    courseExams: [{
+        que: {
+            type: String,
+        },
+        options: [
+            {
+                optionNumber: {
+                    type: String,
+                },
+                val: {
+                    type: String,
+                }
+            }
+        ]
+    }],
+    examSubmissions: [{
+        studentId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Student',
+        },
+        studentName: {
+            type: String,
+        },
+        answers: [{
+            queNo: {
+                type: Number,
+            },
+            solution: {
+                type: Number
+            }
+        }],
+        studentResult: {
+            isAvailable: {
+                type: Boolean,
+                default: false
+            },
+            percentage: {
+                type: Number,
+                default: 100
+            },
+        }
+    }]
+});
+
+const Course = mongoose.model('Course', courseSchema);
+
+module.exports = { Course}
