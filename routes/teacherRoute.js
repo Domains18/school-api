77ca6c3e4460bc8165e67748864ea93d34b12421
Domains18 <gibsonsgibson88@gmail.com>
2023-01-28 18:30:10 +0300

    memo array

--- routes/teacherRoute.js
+++ routes/teacherRoute.js
@@ -5,9 +5,21 @@ const memoSchema = require("/models/memoSchema");
 const { Course } = require("/models/courseSchema");
 
 
-router.get("/memo", async (req, res) => {
+router.get("/allStudents", async (req, res) => {
     let allStudents = await Student.find();
-    
+
     return res.json(allStudents);
-    
+
+});
+
+router.post("/memo", async (req, res) => {
+    setTimeout(async () => {
+        const { teacherId } = req.body;
+        let teacherProfile = await TeacherProfile.findOne({ teacherId });
+        let memos = [];
+        for (i = 0; i < teacherProfile.memo.length; i++){
+            let memo = await memoSchema.findById(teacherProfile.memos.push(memo)); 
+        }
+        
+    });
 });
