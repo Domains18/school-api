46cadf2872e58264c0e641c7bddc7d484bb54d4a
Domains18 <gibsonsgibson88@gmail.com>
2023-01-28 18:16:30 +0300

    dependencies for teacher route

--- routes/teacherRoute.js
+++ routes/teacherRoute.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-
 const { Student } = require("/models/registerSchema");
-
 const { TeacherProfile, StudentProfile } = require("/models/profileSchema");
+const memoSchema = require("/models/memoSchema");
+const { Course } = require("/models/courseSchema");
