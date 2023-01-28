ce63f3f8122572e43467c0f803a69385b1c21606
Domains18 <gibsonsgibson88@gmail.com>
2023-01-28 18:18:58 +0300

    memo functionality

--- routes/teacherRoute.js
+++ routes/teacherRoute.js
@@ -3,3 +3,11 @@ const { Student } = require("/models/registerSchema");
 const { TeacherProfile, StudentProfile } = require("/models/profileSchema");
 const memoSchema = require("/models/memoSchema");
 const { Course } = require("/models/courseSchema");
+
+
+router.get("/memo", async (req, res) => {
+    let allStudents = await Student.find();
+    
+    return res.json(allStudents);
+    
+});
