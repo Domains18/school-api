b29b956499001c68064d24cacbcb47a512ff944b
Domains18 <gibsonsgibson88@gmail.com>
2023-01-28 17:57:18 +0300

    student saving logic

--- routes/register.js
+++ routes/register.js
@@ -1,4 +1,15 @@
 const router = require('express').Router();
 
-const { Student, Teacher, Guardian } = require("../models/registerSchema")
-const {}
+const { Student, Teacher, Guardian } = require("../models/registerSchema");
+const { TeacherProfile, StudentProfile, ParentProfile } = require("../models/profileSchema");
+
+router.post("/student", async (req, res) => {
+    const { name, email, password } = req.body;
+    const student = new Student({ name, email, password });
+    student.save();
+
+    const studentProfile = new StudentProfile({ studentId: student._id, studentName: name });
+    studentProfile.save()
+    res.json("Student details succesfully recorded");
+    
+});
